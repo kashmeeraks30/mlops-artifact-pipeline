a786550bbeb4b00780abf8b838ffb800bbe47fdd
kashmeeraks30 <g24ai1091@iitj.ac.in>
2025-07-16 05:21:24 +0000

    modified the code to use the utility functions from utils.py

--- src/train.py
+++ src/train.py
@@ -6,16 +6,7 @@ from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import os
-
-def load_config(config_path='config/config.json'):
-    with open(config_path,'r') as f:
-        config=json.load(f)
-    return config
-
-def train_model(X,y,config):
-    model=LogisticRegression(C=config['C'],solver=config['solver'],max_iter=config['max_iter'],random_state=42)
-    model.fit(X,y)
-    return model
+from utils import (load_config,train_model)
 
 def main():
     digits = load_digits()
