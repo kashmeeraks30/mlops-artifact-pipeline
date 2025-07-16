d0acfd8d2c9d01ee22b0ce443cfc8615cade316f
kashmeeraks30 <g24ai1091@iitj.ac.in>
2025-07-16 05:17:33 +0000

    moved utility functions from train.py to utils.py

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from sklearn.datasets import load_digits
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+
+def load_config(config_path='config/config.json'):
+    with open(config_path,'r') as f:
+        config=json.load(f)
+    return config
+
+def train_model(X,y,config):
+    model=LogisticRegression(C=config['C'],solver=config['solver'],max_iter=config['max_iter'],random_state=42)
+    model.fit(X,y)
+    return model
